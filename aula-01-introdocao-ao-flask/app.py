a58a7f5609d718267da732b16da781dbee8a33ff
RICHARD MATHEUS GOMES ANTUNES <richard.antunes@e239.local>
2026-02-23 13:46:22 -0300

    iniciando site em flask

---
+++ aula-01-introdocao-ao-flask/app.py
@@ -0,0 +1,28 @@
+#Comentario no python
+
+#Importando o flask para a aplicação
+from flask import Flask, render_template
+#carregando o Flask na variavel "app"
+#declarando variavel no python
+app = Flask(__name__, template_folder='views')
+#variaveis com __ são variaveis de ambiente do python
+#__name__ representa o nome da aplicação
+
+#CRIANDO A ROTA PRINCIPAL DO SITE
+@app.route('/')
+#def cria funções no python ]
+def home():
+    return render_template('index.html')
+@app.route('/games')
+def games():
+    return render_template('games.html')
+@app.route('/consoles')
+def consoles():
+    return render_template('consoles.html')
+
+
+#iniciando o servidor na porta 5000
+if __name__ == '__main__':
+# if esta verificando se o arquivo gravado em __name__ é o arquivo prinicipal
+    app.run(port=5000, debug=True)
+# o metodo .run() inicia o servidor
