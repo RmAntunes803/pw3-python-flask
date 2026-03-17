85b1c71a118f29fb2d8731c1e9324094c7a546b6
RICHARD MATHEUS GOMES ANTUNES <richard.antunes@e239.local>
2026-03-17 11:57:39 -0300

    aula-03-arquitetura-mvc

---
+++ aula-03-arquitetura-MVC-e-request/app.py
@@ -0,0 +1,25 @@
+#Comentario no python
+
+#Importando o flask para a aplicação
+from flask import Flask, render_template
+
+#Importando o Controller
+from controllers import routes
+#carregando o Flask na variavel "app"
+#declarando variavel no python
+app = Flask(__name__, template_folder='views')
+#variaveis com __ são variaveis de ambiente do python
+#__name__ representa o nome da aplicação
+
+#Enviando a variável app para as rotas
+routes.init_app(app)
+
+
+
+
+
+#iniciando o servidor na porta 5000
+if __name__ == '__main__':
+# if esta verificando se o arquivo gravado em __name__ é o arquivo prinicipal
+    app.run(port=5000, debug=True)
+# o metodo .run() inicia o servidor
---
+++ aula-03-arquitetura-MVC-e-request/controllers/routes.py
@@ -0,0 +1,47 @@
+#Importando o flask para a aplicação
+from flask import  render_template, request
+
+#Criandoa a função principal para inicializar as rotas
+def init_app(app):
+    #Variaveis globais
+    listaConsoles = ['Playstation 5', 'xbox One', 'Super Nintendo', 'Atari', '3DS',]
+    
+    
+        #CRIANDO A ROTA PRINCIPAL DO SITE
+    @app.route('/')
+    #def cria funções no python ]
+    def home():
+        return render_template('index.html')
+    @app.route('/games')
+    def games():
+        # Criando  variáveis para a rota de games
+        titulo = "Portal 2"
+        ano = 2011
+        categoria = "Puzzle"
+        #Lista de jogadores (uma lista é um vetor/array)
+        jogadores = ['Marcos', 'Richard', 'Miguel', 'Renato', 'Pedro']
+        # enviando as variaveis para op html
+        
+        return render_template('games.html',
+                            titulo = titulo,
+                            ano = ano,
+                            categoria = categoria,
+                            jogadores=jogadores )
+
+    @app.route('/consoles', methods=['GET', 'POST'])
+    def consoles():
+        # Criando um objeto
+        console = {"Nome" : "Playstation 2",
+                "Fabricante" : "Sony",
+                "Ano" : 2000}
+       
+        
+        #Recebendo o valor do formulário
+        if request.method == 'POST':
+            if request.form.get('novoConsole'):
+                listaConsoles.append(request.form.get('novoConsole'))
+                
+
+        return render_template('consoles.html',
+                            console = console,
+                            listaConsoles = listaConsoles)
