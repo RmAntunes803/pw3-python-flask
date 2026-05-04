20a52ff6d786c3d27bd823f4e8e37820f0087937
RICHARD MATHEUS GOMES ANTUNES <richard.antunes@e239.local>
2026-05-04 13:57:25 -0300

    aula-05.1-CRUD-Lendo-dados-do-banco

---
+++ aula-05.1-CRUD-Lendo-dados-do-banco/controllers/routes.py
@@ -0,0 +1,72 @@
+#Importando o flask para a aplicação
+from flask import  render_template, request, redirect, url_for
+# importando o Model de Games
+from models.database import Game
+
+
+#Criandoa a função principal para inicializar as rotas
+def init_app(app):
+    #Variaveis globais
+    listaConsoles = ['Playstation 5', 'xbox One', 'Super Nintendo', 'Atari', '3DS',]
+    
+    listaGames = [{'titulo' : 'CS-GO', 'ano': 2012, 'categoria': 'FPS online', 'plataforma': 'PC (Windows)'}]
+    
+    
+        #CRIANDO A ROTA PRINCIPAL DO SITE
+    @app.route('/')
+    #def cria funções no python ]
+    def home():
+        return render_template('index.html')
+    @app.route('/games')
+    def games():
+        # Criando  variáveis para a rota de games
+        titulo = "Portal 2"
+        ano = 2011
+        categoria = "Puzzle"
+        #Lista de jogadores (uma lista é um vetor/array)
+        jogadores = ['Marcos', 'Richard', 'Miguel', 'Renato', 'Pedro']
+        # enviando as variaveis para op html
+        
+        return render_template('games.html',
+                            titulo = titulo,
+                            ano = ano,
+                            categoria = categoria,
+                            jogadores=jogadores )
+
+    @app.route('/consoles', methods=['GET', 'POST'])
+    def consoles():
+        # Criando um objeto
+        console = {"Nome" : "Playstation 2",
+                "Fabricante" : "Sony",
+                "Ano" : 2000}
+       
+        
+        #Recebendo o valor do formulário
+        if request.method == 'POST':
+            if request.form.get('novoConsole'):
+                listaConsoles.append(request.form.get('novoConsole'))
+                
+
+        return render_template('consoles.html',
+                            console = console,
+                            listaConsoles = listaConsoles)
+        
+    @app.route("/cadgames", methods=['GET', 'POST'])
+    def cadgames():
+        # recebendo os dados do formulario e enviando para pagina
+        # verificando se a requisição do usuario é do tipo POST
+        if request.method == 'POST':
+            # aqui ele ira gravar a lista d ejogos
+            listaGames.append({'titulo' : request.form.get('titulo'), 'ano' : request.form.get('ano'), 'categoria' : request.form.get('categoria'), 'plataforma': request.form.get('plataforma')})
+            # Aqui o usuário será redireionado novamente para página
+            return redirect(url_for('cadgames'))
+            
+        return render_template('cadgames.html',
+                                listaGames = listaGames)
+        
+        #ROTA PARA O CRUD (ESTOQUE DE JOGOS)
+    @app.route('/estoque', methods=['GET', 'POST'])
+    def estoque():
+        # SELCIONANDO TODOS OS JOGOS DA TABELA
+        games = Game.query.all()
+        return render_template('estoque.html', games=games)
---
+++ aula-05.1-CRUD-Lendo-dados-do-banco/models/database.py
@@ -0,0 +1,29 @@
+# importando o flask-SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+
+# Carregando o SQLAlchemy em uma variável
+db = SQLAlchemy()
+
+# Criando uma classe para representar a entidade Games no banco
+# Nome de classe sempre no singular(boa pratica)
+class Game(db.Model):
+    # Definindo os atributos (colunas) da tabela
+    # Schema (estrutura a baixo)
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(150))
+    ano = db.Column(db.Integer)
+    categoria = db.Column(db.String(150))
+    plataforma = db.Column(db.String(150))
+    preco = db.Column(db.Float)
+    quantidade = db.Column(db.Integer)
+    
+    # Incializando as variáveis a classe (metodo construtor)
+    #self parametro que significa a si mesmo
+    def __init__(self, titulo, ano, categoria, plataforma, preco, quantidade):
+        self.titulo = titulo
+        self.ano = ano
+        self.categoria = categoria
+        self.plataforma = plataforma
+        self.preco = preco 
+        self.quantidade = quantidade
+        
